006777cbf26259443b0551edc8b9d1120e6ce775
Ghanshyam Joshi <47424158+Ghansu@users.noreply.github.com>
2020-09-23 06:44:21 -0700

    Unity2D Practice

---
+++ SnakeController.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    public float speed = 3f;
+    public float rotationSpeed = 200f;
+
+    float horizonatal = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizonatal = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.up * speed * Time.fixedDeltaTime, Space.Self);
+        transform.Rotate(Vector3.forward * -horizonatal * rotationSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+    }
+}
---
+++ Trail.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(EdgeCollider2D))]
+public class Trail : MonoBehaviour
+{
+    public Transform snake;
+    public float pointSpacing = 0.1f; 
+    List<Vector2> points;
+
+    LineRenderer line;
+    EdgeCollider2D col;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        col = GetComponent<EdgeCollider2D>();
+
+        points = new List<Vector2>();
+
+        SetPoint();
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(points.Last(), snake.position) > pointSpacing)
+        {
+            SetPoint();
+        }
+    }
+
+    void SetPoint()
+    {
+        if (points.Count > 1)
+        {
+            col.points = points.ToArray<Vector2>();
+        }
+
+        points.Add(snake.position);
+
+        line.positionCount = points.Count;
+        line.SetPosition(points.Count - 1, snake.position);
+    }
+}
